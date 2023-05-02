55fe8ba41cfcc4670f56bbc62b58ad3b0b8501bd
Irinka2203 <irinkah2203@gmail.com>
2023-05-02 13:17:34 +0300

    feat: add a solution

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(
+    path.join(__dirname, 'text.txt'),
+    'utf-8',
+    (err, text) => {
+        if (err) throw err;
+        console.log(text);
+    }
+);
