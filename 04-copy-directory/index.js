9b60b41122541f2368594804d8f0bd939a38bc4a
Irinka2203 <irinkah2203@gmail.com>
2023-05-06 11:01:00 +0300

    fix: change solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,28 +2,32 @@ const fs = require('fs');
 const path = require('path');
 
 function copiDir() {
-  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
     if (err) {
       console.log(err);
     }
     
-    fs.readdir(path.join(__dirname, 'files'),  (err, files) => {
+    fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, (err) => {
       if (err) {
         console.log(err);
       }
-      console.log(files)
-      files.forEach((file) => {
+  
+      fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+        if (err) {
+          console.log(err);
+        }
+    
+        files.forEach((file) => {
         
-        fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
-          if (err) {
-            console.log(err);
-          }
-          console.log(file)
+          fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+            if (err) {
+              console.log(err);
+            }
+          })
         })
       })
     })
-  })
-  
+  })  
 }
 
 copiDir()
